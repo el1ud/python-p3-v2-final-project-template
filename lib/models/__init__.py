03207af125e26ac569d89e086b061abfdd01b4ed
eliud wainaina <wainainaeliud7@gmail.com>
2024-07-03 16:49:59 -0700

    updated the innit

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,17 +1,7 @@
-# import sqlite3
-
-# CONN = sqlite3.connect('company.db')
-# CURSOR = CONN.cursor()
 from .food import Food
 from .menu import Menu
 
 import sqlite3
 
-# Establishing a database connection
-try:
-    CONN = sqlite3.connect('company.db')
-    CURSOR = CONN.cursor()
-    print("Database connection established successfully.")
-except sqlite3.Error as e:
-    print(f"An error occurred: {e}")
-    
+CONN = sqlite3.connect('company.db')
+CURSOR = CONN.cursor()
